7035512978773037bac0672ae64d313742220feb
Julius Reade <julius.reade@gmail.com>
2024-05-31 07:06:14 +1000

    feat: rearrange project files

--- Models/Model.cs
+++ Models/Model.cs
@@ -19,7 +19,7 @@ public class BloggingContext : DbContext
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+        => options.UseSqlite($"Data Source={"blogging.db"}");
 }
 
 public class Blog
